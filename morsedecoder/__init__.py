203186f7d506f3d7a7d38a0460006f5b5a891c5c
Kaktus <daskaktus@gmail.com>
2020-06-09 08:57:49 +0200

    First Commit

---
+++ morsedecoder/__init__.py
@@ -0,0 +1,4 @@
+from .morsedecoder import Morsedecoder
+
+def setup(bot):
+    bot.add_cog(Morsedecoder())
---
+++ morsedecoder/morsedecoder.py
@@ -0,0 +1,12 @@
+from redbot.core import commands
+
+class Morsedecoder(commands.Cog):
+    """Morse Decoder cog"""
+
+    @commands.command()
+    async def decode(self, ctx):
+        """Tries to decode morse from attached audio or video"""
+        msg = copy(ctx.message)
+        files: List[discord.File] = await Tunnel.files_from_attach(msg)
+        for f in files
+            await ctx.send(f)
