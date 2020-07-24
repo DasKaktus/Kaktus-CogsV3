83f4975784772b09befb44b65c9bd3e5d5f111a4
Kaktus <daskaktus@gmail.com>
2020-07-24 16:19:36 +0200

    Update custcomimproved.py

--- custcomimproved/custcomimproved.py
+++ custcomimproved/custcomimproved.py
@@ -557,11 +557,11 @@ class CustomCommandsImproved(commands.Cog):
 
     async def cc_command(self, ctx, *cc_args, raw_response, **cc_kwargs) -> None:
         cc_args = (*cc_args, *cc_kwargs.values())
-        results = re.findall(r"{([^}]+)\}/i", raw_response)
+        results = re.findall(r"{([^}]+)\}", raw_response)
         for result in results:
             param = self.transform_parameter(result, ctx.message)
             raw_response = raw_response.replace("{" + result + "}", param)
-        results = re.findall(r"{((\d+)[^.}]*(\.[^:}]+)?[^}]*)\}/i", raw_response)
+        results = re.findall(r"{((\d+)[^.}]*(\.[^:}]+)?[^}]*)\}", raw_response)
         if results:
             low = min(int(result[1]) for result in results)
             for result in results:
@@ -569,6 +569,7 @@ class CustomCommandsImproved(commands.Cog):
                 arg = self.transform_arg(result[0], result[2], cc_args[index])
                 raw_response = raw_response.replace("{" + result[0] + "}", arg)
         await ctx.send(raw_response)
+        await ctx.send("tt")
 
     @staticmethod
     def prepare_args(raw_response) -> Mapping[str, Parameter]:
