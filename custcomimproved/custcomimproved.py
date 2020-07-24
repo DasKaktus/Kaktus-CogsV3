ddcbc194d85ef28e8a4dbb24cc132f3968172655
Kaktus <daskaktus@gmail.com>
2020-07-24 16:33:37 +0200

    Update custcomimproved.py

--- custcomimproved/custcomimproved.py
+++ custcomimproved/custcomimproved.py
@@ -85,9 +85,8 @@ class CommandObjImproved:
         # in the ccinfo dict
         return "{:%d/%m/%Y %H:%M:%S}".format(datetime.utcnow())
 
-    async def get(self, message: discord.Message, command: str) -> Tuple[str, Dict]:
+    async def get(self, message: discord.Message, command: str.lower) -> Tuple[str.lower, Dict]:
         if not command:
-            await ctx.send("9")
             raise NotFound()
         ccinfo = await self.db(message.guild).commands.get_raw(command.lower, default=None)
         if not ccinfo:
