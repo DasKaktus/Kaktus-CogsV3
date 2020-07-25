63d24892e52cd54a4bc53547bfd235a5f9066029
Kaktus <daskaktus@gmail.com>
2020-07-25 16:23:53 +0200

    Update enrichmentcenter.py

--- enrichmentcenter/enrichmentcenter.py
+++ enrichmentcenter/enrichmentcenter.py
@@ -100,7 +100,7 @@ class EnrichmentCenter(commands.Cog):
         channel = ctx.channel
         #settings = await self.config.guild(ctx.guild).all()
         
-        if user.id in self.config.guild(ctx.guild).UserProgress(:
+        if user.id in self.config.guild(ctx.guild).UserProgress():
             await ctx.send("Pass")
             pass
         else:
