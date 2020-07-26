88afa1ee34217ac9e4d66fe36d822c02dc8721a0
Kaktus <daskaktus@gmail.com>
2020-07-26 02:44:40 +0200

    Update enrichmentcenter.py

--- enrichmentcenter/enrichmentcenter.py
+++ enrichmentcenter/enrichmentcenter.py
@@ -185,16 +185,16 @@ Please proceed to the chamberlock. >_________________> . Mind the gap."""
             
             #await ctx.send(message.embeds[0].footer.text)
             
-            embedmsg = message.embeds[0]
+            org_msg = message.embeds[0].fields[0].value
             
             
             tid = int(message.embeds[0].footer.text.split(":")[1].split()[0])
             tid = tid - 1
             
-            newembed = discord.Embed(embedmsg)
+            #newembed = discord.Embed(embedmsg)
             
-            #newembed = discord.Embed(color=0xEE2222, title='Test')
-           # newembed.add_field(name='Computer output', value=org_msg)
+            newembed = discord.Embed(color=0xEE2222, title='Test')
+            newembed.add_field(name='Computer output', value=org_msg)
             newembed.set_footer(text="This message will selfdestruct in: {} seconds".format(tid))
             await message.edit(embed=newembed)
             #message.content
