d495c8bbaa6f7aa348e062937f9f96e0f49c9576
Kaktus <daskaktus@gmail.com>
2020-09-16 12:30:46 +0200

    Update __init__.py

--- stattracker/__init__.py
+++ stattracker/__init__.py
@@ -1,6 +1,8 @@
 from .stattracker import Stattracker
 import pathlib
 
+path = 'data/kaktuscog/stattracker
+
 def setup(bot):
     pathlib.Path(path).mkdir(exist_ok=True, parents=True)
     cog = Stattracker(bot)
